8be193238b7b69815d061ccbd508e360a3450a71
kimval2 <kimberly2224@hotmail.com>
2025-02-05 18:56:56 -0800

    feat(weather): New components
    Add design and styles.

---
+++ weather-app/src/components/Search/Search.tsx
@@ -0,0 +1,11 @@
+
+function Search() {
+  return (
+    <div className="search_wrapper">
+      <input type="text" />
+      <button type="button">Search</button>
+    </div>
+  );
+}
+
+export default Search;
--- weather-app/src/pages/Weather.tsx
+++ weather-app/src/pages/Weather.tsx
@@ -1,4 +1,6 @@
 import {useState} from "react";
+import './weather.scss';
+import Search from "../components/Search/Search";
 
 function Weather() {
     const [city, setCity] = useState<string>('');
@@ -6,7 +8,34 @@ function Weather() {
     const [loading, setLoading] = useState<boolean>(false);
     const [error, setError] = useState<string | null>(null);
 
-    return (<>...</>);
+    return (<>
+      <div className="weather_card">
+       <div className="search_wrapper">
+        <Search />
+       </div>
+       <div className="weather_content">
+        <div className="weather_details">
+          <div className="weather_details--current">
+            <h2>City Name</h2>
+            <p>Weather Description</p>
+            <p>Temperature</p>
+            <p>Humidity</p>
+            <p>Wind Speed</p>
+          </div>
+        </div>
+        <div className="weather_details weather_details--daily">
+          <h2>5 Day Forecast</h2>
+          <div className="weather_details--daily__item">
+            <p>Day of the week</p>
+            <p>Weather Description</p>
+            <p>Temperature</p>
+            <p>Humidity</p>
+            <p>Wind Speed</p>
+          </div>
+        </div>
+       </div>
+      </div>
+    </>);
 }
 
 export default Weather;
