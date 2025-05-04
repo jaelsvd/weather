bc99269fc309dc3bdc29a7599391cb8e314644bd
Jael <Jael.svd@gmail.com>
2025-05-04 12:10:08 -0700

    remove deployment errors

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,8 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './styles/main.scss'
 import './App.css'
 import Weather from './pages/Weather'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
